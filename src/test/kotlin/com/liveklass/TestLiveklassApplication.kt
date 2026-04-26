3df7b1a4d43e0d63d8d54aa7f2cc4456f75788f1
hodako <hodako2401@gmail.com>
2026-04-26 14:32:36 +0900

    chore(test): 불필요한 설정 삭제

--- src/test/kotlin/com/liveklass/TestLiveklassApplication.kt
+++
@@ -1,8 +0,0 @@
-package com.liveklass
-
-import org.springframework.boot.fromApplication
-import org.springframework.boot.with
-
-fun main(args: Array<String>) {
-    fromApplication<LiveklassApplication>().with(TestcontainersConfiguration::class).run(*args)
-}
