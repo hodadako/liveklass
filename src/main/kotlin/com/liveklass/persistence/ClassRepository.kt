ae789503ec5d4d84e8135dd3e4c34f0dd911adc4
hodako <hodako2401@gmail.com>
2026-04-26 14:58:35 +0900

    feat(class): 강의 목록 조회 기능 추가

--- src/main/kotlin/com/liveklass/persistence/ClassRepository.kt
+++ src/main/kotlin/com/liveklass/persistence/ClassRepository.kt
@@ -1,6 +1,7 @@
 package com.liveklass.persistence
 
 import com.liveklass.domain.Class
+import com.liveklass.domain.ClassStatus
 import jakarta.persistence.LockModeType
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.jpa.repository.Lock
@@ -10,4 +11,6 @@ interface ClassRepository : JpaRepository<Class, Long> {
     @Lock(LockModeType.PESSIMISTIC_WRITE)
     @Query("SELECT c FROM Class c WHERE c.id = :id")
     fun findByIdForUpdate(id: Long): Class?
+
+    fun findAllByClassStatus(classStatus: ClassStatus): List<Class>
 }
--- src/test/kotlin/com/liveklass/persistence/ClassRepositoryTest.kt
+++ src/test/kotlin/com/liveklass/persistence/ClassRepositoryTest.kt
@@ -1,6 +1,7 @@
 package com.liveklass.persistence
 
 import com.liveklass.IntegrationTestSupport
+import com.liveklass.domain.ClassStatus
 import com.liveklass.fixture.DomainTestFixture.getClassFixture
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.DisplayName
@@ -16,7 +17,7 @@ class ClassRepositoryTest : IntegrationTestSupport() {
     private lateinit var classRepository: ClassRepository
 
     @Test
-    fun `강의를 저장하면 Auditing 필드가 기록된다`() {
+    fun `강의를 저장하면 Auditing 필드가 기록되고 id가 생성된다`() {
         val klass = getClassFixture()
 
         val savedClass = classRepository.saveAndFlush(klass)
@@ -29,8 +30,26 @@ class ClassRepositoryTest : IntegrationTestSupport() {
     }
 
     @Test
+    fun `특정 강의 상태로 조회시 해당하는 강의 목록을 반환한다`() {
+        val testClass1 = getClassFixture()
+        val testClass2 = getClassFixture()
+        testClass2.open()
+
+        classRepository.save(testClass1)
+        classRepository.save(testClass2)
+
+        val foundClasses = classRepository.findAllByClassStatus(ClassStatus.DRAFT)
+
+        assertAll(
+            { assertThat(foundClasses.size).isEqualTo(1) },
+            { assertThat(foundClasses[0].id).isNotZero() },
+            { assertThat(foundClasses[0].classStatus).isEqualTo(ClassStatus.DRAFT) }
+        )
+    }
+
+    @Test
     @Transactional
-    fun `비관적 락으로 강의를 조회할 수 있다`() {
+    fun `비관적 락을 통해서 강의를 조회할 수 있다`() {
         val savedClass = classRepository.saveAndFlush(getClassFixture())
 
         val foundClass = classRepository.findByIdForUpdate(savedClass.id)
